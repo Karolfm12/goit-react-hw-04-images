d85b76c1d24b122aa29e885564f1a513851897a8
Karolfm12 <karolfm12@onet.eu>
2024-04-21 21:15:47 +0200

    firstCommit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { useState } from 'react';
+import { INITIAL_VALUES } from './initial-values';
+
 export const App = () => {
+  const [data, setData] = useState(INITIAL_VALUES);
+
+  const handleOnChange = e => {
+    // setData({ q: e.target.value });
+    setData(prev => ({
+      ...prev,
+      q: e.target.value,
+    }));
+  };
+  const handleOnSubmit = () => {};
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <form onSubmit={handleOnSubmit}>
+        <input type="text" onChange={handleOnChange}></input>
+        <button type="submit">Search</button>
+      </form>
+      <p>{data.q}</p>
+    </>
   );
 };
---
+++ src/components/initial-values.js
@@ -0,0 +1,10 @@
+export const INITIAL_VALUES = {
+  key: '18941965-072e6ae370689f800c64fac36',
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 10,
+  page: 1,
+  imageData: [],
+};
